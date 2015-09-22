0ff8995664704b0db68e15ad0a7b534b1155fd23
Thúlio Costa <thulio@thuliocosta.com>
2015-09-21 22:42:08 -0300

    Rename docker-phoenix to alpine-phoenix

--- phoenix_example/build.sh
+++ phoenix_example/build.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
+PROJECT_PATH=$(dirname "$0")
+pushd "$PROJECT_PATH/../alpine-phoenix" > /dev/null
+./build.sh alpine-phoenix
+popd > /dev/null
+pushd $PROJECT_PATH >/dev/null
 rm -rf app
-mix phoenix.new --no-ecto --no-brunch app
+yes | mix phoenix.new --no-ecto --no-brunch app
 docker build --rm -t 'phoenix-example' .
 docker run --rm -ti -p 4000:4000 phoenix-example sh
 docker rmi 'phoenix-example'
+rm -rf app
